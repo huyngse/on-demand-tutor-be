24c6f60bd108bbc200691ddcd8f243b144222bf3
phamnhatlinh <phamnhatlinh20303@gmail.com>
2024-05-24 14:12:58 +0700

    swagger UI fixing

--- BE_SWP391_OnDemandTutor.API/Program.cs
+++ BE_SWP391_OnDemandTutor.API/Program.cs
@@ -1,29 +1,52 @@
+
 using BE_SWP391_OnDemandTutor.BusinessLogic.Generations.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using BE_SWP391_OnDemandTutor.API.GlobalExceptionHandler;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using BirdCageShop.Presentation.AutoMapperConfig;
+using BE_SWP391_OnDemandTutor.DataAccess.Data;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq(serverUrl: "http://localhost:5341"));
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new OpenApiInfo() { Title = "On Tutor Demand", Version = "v1" });
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.ConfigureAutoMapper();
-builder.Services.InitializerDependencyInjection();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(config =>
+    {
+        config.SwaggerEndpoint("/swagger/v1/swagger.json", "On Tutor Demand");
+    });
 }
+
+app.UseCors();
+
+
+//app.UseMiddleware<UnauthourizeExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
