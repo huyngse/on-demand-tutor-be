bc5ba3be74663dbab3983e6580eb4b1febf4f504
phamnhatlinh <phamnhatlinh20303@gmail.com>
2024-06-07 00:56:59 +0700

    Schedule OK

--- BE_SWP391_OnDemandTutor.BusinessLogic/RequestModels/Schedule/CreateScheduleRequestModel.cs
+++ BE_SWP391_OnDemandTutor.BusinessLogic/RequestModels/Schedule/CreateScheduleRequestModel.cs
@@ -3,7 +3,10 @@ namespace BE_SWP391_OnDemandTutor.BusinessLogic.RequestModels.Schedule
 {
 
    public class CreateScheduleRequestModel {
-     
-   }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
 
 }
