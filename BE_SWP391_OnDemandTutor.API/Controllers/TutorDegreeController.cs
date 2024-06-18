5d637207213b48db9d45c26ecc5865aaa9858c4c
phamnhatlinh <phamnhatlinh20303@gmail.com>
2024-06-18 15:27:38 +0700

    Hot fix

--- BE_SWP391_OnDemandTutor.API/Controllers/TutorDegreeController.cs
+++ BE_SWP391_OnDemandTutor.API/Controllers/TutorDegreeController.cs
@@ -11,77 +11,7 @@ namespace BE_SWP391_OnDemandTutor.Presentation.Controllers
     [Route("/api/v1/tutordegrees")]
     public class TutorDegreeController : ControllerBase {
 
-        private ITutorDegreeService _tutordegreeService;
-
-         public TutorDegreeController(ITutorDegreeService tutordegreeService)
-        {
-            _tutordegreeService = tutordegreeService;
-        }
-
-        [MapToApiVersion("1")]
-        [HttpPost]
-        public ActionResult<TutorDegreeViewModel> CreateTutorDegree(CreateTutorDegreeRequestModel tutordegreeCreate)
-        {
-            var tutordegreeCreated = _tutordegreeService.CreateTutorDegree(tutordegreeCreate);
-
-            if (tutordegreeCreated == null)
-            {
-                return NotFound("");
-            }
-            return tutordegreeCreated;
-        }
-
-        [MapToApiVersion("1")]
-        [HttpGet]
-        public ActionResult<List<TutorDegreeViewModel>> GetAll()
-        {
-            var tutordegreeList = _tutordegreeService.GetAll();
-
-            if (tutordegreeList == null)
-            {
-                return NotFound("");
-            }
-            return tutordegreeList;
-        }
-
-        [MapToApiVersion("1")]
-        [HttpGet("idTmp")]
-        public ActionResult<TutorDegreeViewModel> GetById(int idTmp)
-        {
-            var tutordegreeDetail = _tutordegreeService.GetById(idTmp);
-
-            if (tutordegreeDetail == null)
-            {
-                return NotFound("");
-            }
-            return tutordegreeDetail;
-        }
-
-        [MapToApiVersion("1")]
-        [HttpDelete]
-        public ActionResult<bool> DeleteTutorDegree(int idTmp)
-        {
-            var check = _tutordegreeService.DeleteTutorDegree(idTmp);
-
-            if (check == false)
-            {
-                return NotFound("");
-            }
-            return check;
-        }
-
-        [MapToApiVersion("1")]
-        [HttpPut]
-        public ActionResult<TutorDegreeViewModel> UpdateTutorDegree(UpdateTutorDegreeRequestModel tutordegreeCreate)
-        {
-            var tutordegreeUpdated = _tutordegreeService.UpdateTutorDegree(tutordegreeCreate);
-
-            if (tutordegreeUpdated == null)
-            {
-                return NotFound("");
-            }
-            return tutordegreeUpdated;
-        }
+     
     }
 
 }
