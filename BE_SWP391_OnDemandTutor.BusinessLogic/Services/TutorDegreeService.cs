d91ee0655c6734ab97e88603ad8ab17fa00af022
cohieu1803 <hieuncse183228@fpt.edu.vn>
2024-06-18 07:50:53 +0700

    Feature/TutorDegree_CRUD

--- BE_SWP391_OnDemandTutor.BusinessLogic/Services/TutorDegreeService.cs
+++ BE_SWP391_OnDemandTutor.BusinessLogic/Services/TutorDegreeService.cs
@@ -1,45 +1,98 @@
+using AutoMapper;
+using BE_SWP391_OnDemandTutor.BusinessLogic.RequestModels.Schedule;
 using BE_SWP391_OnDemandTutor.BusinessLogic.RequestModels.TutorDegree;
 using BE_SWP391_OnDemandTutor.BusinessLogic.ViewModels;
+using BE_SWP391_OnDemandTutor.DataAccess.Models;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BE_SWP391_OnDemandTutor.BusinessLogic.Services
 {
 
     public interface ITutorDegreeService
     {
-        public TutorDegreeViewModel CreateTutorDegree(CreateTutorDegreeRequestModel tutordegreeCreate);
-        public TutorDegreeViewModel UpdateTutorDegree(UpdateTutorDegreeRequestModel tutordegreeUpdate);
-        public bool DeleteTutorDegree(int idTmp);
-        public List<TutorDegreeViewModel> GetAll();
-        public TutorDegreeViewModel GetById(int idTmp);
+        Task<TutorDegreeViewModel> GetById(int idTmp);
+        Task<List<TutorDegreeViewModel>> GetAll();
+        Task<TutorDegreeViewModel> CreateTutorDegree(CreateTutorDegreeRequestModel scheduleCreate);
+        Task<TutorDegreeViewModel> UpdateDegree(UpdateTutorDegreeRequestModel scheduleUpdate);
+        Task<bool> DeleteTutorDegree(int idTmp);
     }
 
     public class TutorDegreeService : ITutorDegreeService
     {
+        private readonly BE_SWP391_OnDemandTutorDbContext _context;
+        private readonly IMapper _mapper;
 
-      
-        public TutorDegreeViewModel CreateTutorDegree(CreateTutorDegreeRequestModel tutordegreeCreate)
+        public TutorDegreeService(BE_SWP391_OnDemandTutorDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public TutorDegreeViewModel UpdateTutorDegree(UpdateTutorDegreeRequestModel tutordegreeUpdate)
+
+        public async Task<TutorDegreeViewModel> CreateTutorDegree(CreateTutorDegreeRequestModel degreeCreate)
         {
-            throw new NotImplementedException();
+
+            var degree = new TutorDegree
+            {
+                DegreeName = degreeCreate.DegreeName,   
+                Description = degreeCreate.Description,
+                DegreeImageUrl = degreeCreate.DegreeImageUrl,
+                TutorId = degreeCreate.TutorId,
+            };
+            _context.TutorDegrees.Add(degree);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<TutorDegreeViewModel>(degree);
         }
 
-        public bool DeleteTutorDegree(int idTmp)
+        public async Task<TutorDegreeViewModel> UpdateDegree(UpdateTutorDegreeRequestModel updateTutor)
         {
-            throw new NotImplementedException();
+            var update = await _context.TutorDegrees.FindAsync(updateTutor.DegreeId);
+            if (update == null)
+            {
+                return null;
+            }
+
+            update.Description = updateTutor.Description;
+            update.DegreeName = updateTutor.DegreeName;
+
+            await _context.SaveChangesAsync();
+            return _mapper.Map<TutorDegreeViewModel>(update);
+
+
         }
 
-        public List<TutorDegreeViewModel> GetAll()
+        public async Task<bool> DeleteTutorDegree(int idTmp)
         {
-            throw new NotImplementedException();
+            var delete = await _context.TutorDegrees.FindAsync(idTmp);
+            if (delete == null)
+            {
+                return false;
+            }
+
+            _context.TutorDegrees.Remove(delete);
+            await _context.SaveChangesAsync();
+            return true;
         }
 
-        public TutorDegreeViewModel GetById(int idTmp)
+        public async Task<List<TutorDegreeViewModel>> GetAll()
         {
-            throw new NotImplementedException();
+            var degree = await _context.TutorDegrees.ToListAsync();
+
+            return degree.Adapt<List<TutorDegreeViewModel>>();
+        }
+
+        public async Task<TutorDegreeViewModel> GetById(int idTmp)
+        {
+            var degree = await _context.TutorDegrees.FindAsync(idTmp);
+            if (degree == null)
+            {
+                return null;
+            }
+            return _mapper.Map<TutorDegreeViewModel>(degree);
+
+
         }
 
     }
