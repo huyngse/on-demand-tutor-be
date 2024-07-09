e4eadba4b027cc17fca90b8bd2e839e5c91bf1b3
phamnhatlinh <phamnhatlinh20303@gmail.com>
2024-07-09 18:58:09 +0700

    Mapper for booking

--- BE_SWP391_OnDemandTutor.BusinessLogic/Services/BookingService.cs
+++ BE_SWP391_OnDemandTutor.BusinessLogic/Services/BookingService.cs
@@ -76,145 +76,21 @@ namespace BE_SWP391_OnDemandTutor.BusinessLogic.Services
                 .ThenInclude(b => b.Tutor)
                 .Include(b => b.User)
                 .ToListAsync();
-            return booking.Select(booking => new BookingDetailViewModel
-            {
-                BookingId = booking.BookingId,
-                CreateDate = booking.CreateDate,
-                Description = booking.Description,
-                Address = booking.Address,
-                StartDate = booking.StartDate,
-                EndDate = booking.EndDate,
-                Status = booking.Status,
-                Schedule = new BookingScheduleViewModel
-                {
-                    ScheduleID = booking.ScheduleId,
-                    DateOfWeek = booking.Schedule.DateOfWeek,
-                    Description = booking.Schedule.Description,
-                    EndTime = booking.Schedule.EndTime,
-                    StartTime = booking.Schedule.StartTime,
-                    Title = booking.Schedule.Title,
-                },
-                User = new BookingUserViewModel
-                {
-                    City = booking.User.City,
-                    DateOfBirth = booking.User.DateOfBirth,
-                    District = booking.User.District,
-                    EmailAddress = booking.User.EmailAddress,
-                    FullName = booking.User.FullName,
-                    Gender = booking.User.Gender,
-                    PhoneNumber = booking.User.PhoneNumber,
-                    ProfileImage = booking.User.ProfileImage,
-                    Street = booking.User.Street,
-                    UserId = booking.User.UserId,
-                    Username = booking.User.Username,
-                    Ward = booking.User.Ward
-                },
-                Class = new BookingClassViewModel
-                {
-                    ClassLevel = booking.Schedule.Class.ClassLevel,
-                    ClassInfo = booking.Schedule.Class.ClassInfo,
-                    ClassId = booking.Schedule.Class.ClassId,
-                    Active = booking.Schedule.Class.Active,
-                    City = booking.Schedule.Class.City,
-                    ClassFee = booking.Schedule.Class.ClassFee,
-                    ClassMethod = booking.Schedule.Class.ClassMethod,
-                    ClassName = booking.Schedule.Class.ClassName,
-                    ClassRequire = booking.Schedule.Class.ClassRequire,
-                    CreatedDate = booking.Schedule.Class.CreatedDate,
-                    District = booking.Schedule.Class.District,
-                    Ward = booking.Schedule.Class.Ward
-                },
-                Tutor = new BookingUserViewModel
-                {
-                    City = booking.Schedule.Class.Tutor.City,
-                    FullName = booking.Schedule.Class.Tutor.FullName,
-                    DateOfBirth = booking.Schedule.Class.Tutor.DateOfBirth,
-                    District = booking.Schedule.Class.Tutor.District,
-                    EmailAddress = booking.Schedule.Class.Tutor.EmailAddress,
-                    Gender = booking.Schedule.Class.Tutor.Gender,
-                    PhoneNumber = booking.Schedule.Class.Tutor.PhoneNumber,
-                    ProfileImage = booking.Schedule.Class.Tutor.ProfileImage,
-                    Street = booking.Schedule.Class.Tutor.Street,
-                    UserId = booking.Schedule.Class.Tutor.UserId,
-                    Username = booking.Schedule.Class.Tutor.Username,
-                    Ward = booking.Schedule.Class.Tutor.Ward
-                }
-            }).ToList();
+            return booking.Select(b => b.Adapt<BookingDetailViewModel>()).ToList();
         }
 
         public async Task<List<BookingDetailViewModel>> GetBookingByTutorId(int tutorId)
         {
-            var booking = await _context.Bookings
-                .Include(b => b.Schedule)
-                .ThenInclude(b => b.Class)
-                .ThenInclude(b => b.Tutor)
-                .Include(b => b.User)
-                .ToListAsync();
-            return booking.Select(booking => new BookingDetailViewModel
-            {
-                BookingId = booking.BookingId,
-                CreateDate = booking.CreateDate,
-                Description = booking.Description,
-                Address = booking.Address,
-                StartDate = booking.StartDate,
-                EndDate = booking.EndDate,
-                Status = booking.Status,
-                Schedule = new BookingScheduleViewModel
-                {
-                    ScheduleID = booking.ScheduleId,
-                    DateOfWeek = booking.Schedule.DateOfWeek,
-                    Description = booking.Schedule.Description,
-                    EndTime = booking.Schedule.EndTime,
-                    StartTime = booking.Schedule.StartTime,
-                    Title = booking.Schedule.Title,
-                },
-                User = new BookingUserViewModel
-                {
-                    City = booking.User.City,
-                    DateOfBirth = booking.User.DateOfBirth,
-                    District = booking.User.District,
-                    EmailAddress = booking.User.EmailAddress,
-                    FullName = booking.User.FullName,
-                    Gender = booking.User.Gender,
-                    PhoneNumber = booking.User.PhoneNumber,
-                    ProfileImage = booking.User.ProfileImage,
-                    Street = booking.User.Street,
-                    UserId = booking.User.UserId,
-                    Username = booking.User.Username,
-                    Ward = booking.User.Ward
-                },
-                Class = new BookingClassViewModel
-                {
-                    ClassLevel = booking.Schedule.Class.ClassLevel,
-                    ClassInfo = booking.Schedule.Class.ClassInfo,
-                    ClassId = booking.Schedule.Class.ClassId,
-                    Active = booking.Schedule.Class.Active,
-                    City = booking.Schedule.Class.City,
-                    ClassFee = booking.Schedule.Class.ClassFee,
-                    ClassMethod = booking.Schedule.Class.ClassMethod,
-                    ClassName = booking.Schedule.Class.ClassName,
-                    ClassRequire = booking.Schedule.Class.ClassRequire,
-                    CreatedDate = booking.Schedule.Class.CreatedDate,
-                    District = booking.Schedule.Class.District,
-                    Ward = booking.Schedule.Class.Ward
-                },
-                Tutor = new BookingUserViewModel
-                {
-                    City = booking.Schedule.Class.Tutor.City,
-                    FullName = booking.Schedule.Class.Tutor.FullName,
-                    DateOfBirth = booking.Schedule.Class.Tutor.DateOfBirth,
-                    District = booking.Schedule.Class.Tutor.District,
-                    EmailAddress = booking.Schedule.Class.Tutor.EmailAddress,
-                    Gender = booking.Schedule.Class.Tutor.Gender,
-                    PhoneNumber = booking.Schedule.Class.Tutor.PhoneNumber,
-                    ProfileImage = booking.Schedule.Class.Tutor.ProfileImage,
-                    Street = booking.Schedule.Class.Tutor.Street,
-                    UserId = booking.Schedule.Class.Tutor.UserId,
-                    Username = booking.Schedule.Class.Tutor.Username,
-                    Ward = booking.Schedule.Class.Tutor.Ward
-                }
-            }).Where(b => b.Tutor.UserId == tutorId).ToList();
-        }
+            var bookings = await _context.Bookings
+               .Include(b => b.Schedule)
+               .ThenInclude(s => s.Class)
+               .ThenInclude(c => c.Tutor)
+               .Include(b => b.User)
+               .Where(b => b.Schedule.Class.TutorId == tutorId)
+               .ToListAsync();
+            return bookings.Select(b => b.Adapt<BookingDetailViewModel>()).ToList();
+        
+    }
         public async Task<List<BookingDetailViewModel>> GetBookingByStudentId(int studentId)
         {
             var booking = await _context.Bookings
@@ -223,70 +99,7 @@ namespace BE_SWP391_OnDemandTutor.BusinessLogic.Services
                 .ThenInclude(b => b.Tutor)
                 .Include(b => b.User)
                 .ToListAsync();
-            return booking.Select(booking => new BookingDetailViewModel
-            {
-                BookingId = booking.BookingId,
-                CreateDate = booking.CreateDate,
-                Description = booking.Description,
-                Address = booking.Address,
-                StartDate = booking.StartDate,
-                EndDate = booking.EndDate,
-                Status = booking.Status,
-                Schedule = new BookingScheduleViewModel
-                {
-                    ScheduleID = booking.ScheduleId,
-                    DateOfWeek = booking.Schedule.DateOfWeek,
-                    Description = booking.Schedule.Description,
-                    EndTime = booking.Schedule.EndTime,
-                    StartTime = booking.Schedule.StartTime,
-                    Title = booking.Schedule.Title,
-                },
-                User = new BookingUserViewModel
-                {
-                    City = booking.User.City,
-                    DateOfBirth = booking.User.DateOfBirth,
-                    District = booking.User.District,
-                    EmailAddress = booking.User.EmailAddress,
-                    FullName = booking.User.FullName,
-                    Gender = booking.User.Gender,
-                    PhoneNumber = booking.User.PhoneNumber,
-                    ProfileImage = booking.User.ProfileImage,
-                    Street = booking.User.Street,
-                    UserId = booking.User.UserId,
-                    Username = booking.User.Username,
-                    Ward = booking.User.Ward
-                },
-                Class = new BookingClassViewModel
-                {
-                    ClassLevel = booking.Schedule.Class.ClassLevel,
-                    ClassInfo = booking.Schedule.Class.ClassInfo,
-                    ClassId = booking.Schedule.Class.ClassId,
-                    Active = booking.Schedule.Class.Active,
-                    City = booking.Schedule.Class.City,
-                    ClassFee = booking.Schedule.Class.ClassFee,
-                    ClassMethod = booking.Schedule.Class.ClassMethod,
-                    ClassName = booking.Schedule.Class.ClassName,
-                    ClassRequire = booking.Schedule.Class.ClassRequire,
-                    CreatedDate = booking.Schedule.Class.CreatedDate,
-                    District = booking.Schedule.Class.District,
-                    Ward = booking.Schedule.Class.Ward
-                },
-                Tutor = new BookingUserViewModel
-                {
-                    City = booking.Schedule.Class.Tutor.City,
-                    FullName = booking.Schedule.Class.Tutor.FullName,
-                    DateOfBirth = booking.Schedule.Class.Tutor.DateOfBirth,
-                    District = booking.Schedule.Class.Tutor.District,
-                    EmailAddress = booking.Schedule.Class.Tutor.EmailAddress,
-                    Gender = booking.Schedule.Class.Tutor.Gender,
-                    PhoneNumber = booking.Schedule.Class.Tutor.PhoneNumber,
-                    ProfileImage = booking.Schedule.Class.Tutor.ProfileImage,
-                    Street = booking.Schedule.Class.Tutor.Street,
-                    UserId = booking.Schedule.Class.Tutor.UserId,
-                    Username = booking.Schedule.Class.Tutor.Username,
-                    Ward = booking.Schedule.Class.Tutor.Ward
-                }
-            }).Where(b => b.User.UserId == studentId).ToList();
+            return booking.Select(b => b.Adapt<BookingDetailViewModel>()).ToList();
         }
         public async Task<BookingViewModel> GetById(int bookingId)
         {
@@ -295,18 +108,7 @@ namespace BE_SWP391_OnDemandTutor.BusinessLogic.Services
             {
                 throw new NotFoundException("Can not find the Booking");
             }
-            return new BookingViewModel
-            {
-                BookingId = booking.BookingId,
-                CreateDate = booking.CreateDate,
-                Description = booking.Description,
-                Address = booking.Address,
-                StartDate = booking.StartDate,
-                EndDate = booking.EndDate,
-                Status = booking.Status,
-                ScheduleId = booking.ScheduleId,
-                UserId = booking.UserId
-            };
+            return booking.Adapt<BookingViewModel>();
         }
         public async Task<BookingDetailViewModel> GetDetailById(int bookingId)
         {
@@ -320,70 +122,7 @@ namespace BE_SWP391_OnDemandTutor.BusinessLogic.Services
             {
                 throw new NotFoundException("Can not find the Booking");
             }
-            return new BookingDetailViewModel
-            {
-                BookingId = booking.BookingId,
-                CreateDate = booking.CreateDate,
-                Description = booking.Description,
-                Address = booking.Address,
-                StartDate = booking.StartDate,
-                EndDate = booking.EndDate,
-                Status = booking.Status,
-                Schedule = new BookingScheduleViewModel
-                {
-                    ScheduleID = booking.ScheduleId,
-                    DateOfWeek = booking.Schedule.DateOfWeek,
-                    Description = booking.Schedule.Description,
-                    EndTime = booking.Schedule.EndTime,
-                    StartTime = booking.Schedule.StartTime,
-                    Title = booking.Schedule.Title,
-                },
-                User = new BookingUserViewModel
-                {
-                    City = booking.User.City,
-                    DateOfBirth = booking.User.DateOfBirth,
-                    District = booking.User.District,
-                    EmailAddress = booking.User.EmailAddress,
-                    FullName = booking.User.FullName,
-                    Gender = booking.User.Gender,
-                    PhoneNumber = booking.User.PhoneNumber,
-                    ProfileImage = booking.User.ProfileImage,
-                    Street = booking.User.Street,
-                    UserId = booking.User.UserId,
-                    Username = booking.User.Username,
-                    Ward = booking.User.Ward
-                },
-                Class = new BookingClassViewModel
-                {
-                    ClassLevel = booking.Schedule.Class.ClassLevel,
-                    ClassInfo = booking.Schedule.Class.ClassInfo,
-                    ClassId = booking.Schedule.Class.ClassId,
-                    Active = booking.Schedule.Class.Active,
-                    City = booking.Schedule.Class.City,
-                    ClassFee = booking.Schedule.Class.ClassFee,
-                    ClassMethod = booking.Schedule.Class.ClassMethod,
-                    ClassName = booking.Schedule.Class.ClassName,
-                    ClassRequire = booking.Schedule.Class.ClassRequire,
-                    CreatedDate = booking.Schedule.Class.CreatedDate,
-                    District = booking.Schedule.Class.District,
-                    Ward = booking.Schedule.Class.Ward
-                },
-                Tutor = new BookingUserViewModel
-                {
-                    City = booking.Schedule.Class.Tutor.City,
-                    FullName = booking.Schedule.Class.Tutor.FullName,
-                    DateOfBirth = booking.Schedule.Class.Tutor.DateOfBirth,
-                    District = booking.Schedule.Class.Tutor.District,
-                    EmailAddress = booking.Schedule.Class.Tutor.EmailAddress,
-                    Gender = booking.Schedule.Class.Tutor.Gender,
-                    PhoneNumber = booking.Schedule.Class.Tutor.PhoneNumber,
-                    ProfileImage = booking.Schedule.Class.Tutor.ProfileImage,
-                    Street = booking.Schedule.Class.Tutor.Street,
-                    UserId = booking.Schedule.Class.Tutor.UserId,
-                    Username = booking.Schedule.Class.Tutor.Username,
-                    Ward = booking.Schedule.Class.Tutor.Ward
-                }
-            };
+            return booking.Adapt<BookingDetailViewModel>();
         }
 
         public async Task<BookingViewModel> UpdateBooking(int bookingId, UpdateBookingViewModel updateModel)
